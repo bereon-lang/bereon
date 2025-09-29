6a26412e44aab9779729544e2db65799ca8f602f
Ygor Simões <ygorsimoes@hotmail.com>
2025-09-28 22:15:03 -0300

    feat: initialize project with basic structure and config

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject bereon "0.1.0-SNAPSHOT"
+  :description "An experimental language"
+  :url "https://github.com/bereon-lang/bereon"
+  :license {:name "MIT License"
+            :url  "https://github.com/bereon-lang/bereon/blob/main/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.12.3"]]
+  :repl-options {:init-ns bereon.core})
---
+++ test/bereon/core_test.clj
@@ -0,0 +1,7 @@
+(ns bereon.core-test
+  (:require [clojure.test :refer :all]
+            [bereon.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
